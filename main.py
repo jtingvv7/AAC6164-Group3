a2d7e64c8b82938847fe11934644886b4dd98c90
Lin Chen Yi <inl56779@gmail.com>
2026-01-27 15:58:16 +0800

    link system_monitor.py with main.py

--- main.py
+++ main.py
@@ -1,33 +1,35 @@
 import time
 import os
+import threading
 from directory_monitor.monitor import DirectoryMonitor
+from system_monitor import start_system_monitor
 
 def main():
     print("Linux Monitoring System Started")
     print("Press Ctrl + C to stop")
-    
+
     watch_dir = "./test_zone"
     if not os.path.exists(watch_dir):
         os.makedirs(watch_dir)
-        print(f"Create testing directoy")
-    
-    # directory monitor
+        print("Created testing directory")
+
     monitor = DirectoryMonitor(watch_dir)
-    
+
+    # Start system monitor in background
+    sys_thread = threading.Thread(target=start_system_monitor, daemon=True)
+    sys_thread.start()
+
     try:
         while True:
-            # check every 3s
             logs = monitor.check_changes()
-
             if logs:
-                print("\n Changes Detected")
+                print("\nChanges Detected")
                 for log in logs:
                     print(log)
-
             time.sleep(3)
 
     except KeyboardInterrupt:
-        print("\n Stopped")
+        print("\nMonitoring stopped")
 
 if __name__ == "__main__":
     main()
